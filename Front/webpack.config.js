4acb3406bb17651baff4419499e71d214ae98387
Adrien <adrien.masson@hotmail.fr>
2020-03-09 13:27:57 +0100

    init

--- Front/webpack.config.js
+++ Front/webpack.config.js
@@ -1,26 +1,31 @@
-const HtmlPlugin = require ('html-webpack-plugin')
+const HtmlPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: './src/index.js',
-    module: {
-        rules : [
-            {
-                test : /\.(js|jsx)$/,
-                use: {
-                    loader: 'babel-loader'
-                },
-                exclude : /node_modules/        
-        },
-        {
-            test : /\.html$/,
-            use: {
-                loader: 'html-loader'
-            }
-        }]
-    },
-    plugins: [
-        new HtmlPlugin({
-            template: 'src/index.html', filename: 'index.output.html'
-        })
-    ]
-  };
+	entry: "./src/index.js",
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				use: {
+					loader: "babel-loader",
+				},
+				exclude: /node_modules/,
+			},
+			{
+				test: /\.html$/,
+				use: {
+					loader: "html-loader",
+				},
+				resolve: {
+					extensions: [".js", ".jsx"],
+				},
+			},
+		],
+	},
+	plugins: [
+		new HtmlPlugin({
+			template: "src/index.html",
+			filename: "index.output.html",
+		}),
+	],
+};
